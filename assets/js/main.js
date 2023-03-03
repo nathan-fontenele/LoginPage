542a4f2c528b97b490a8a76eea9de3fb8f60e324
Nathan Fontenele <nathangf60@outlook.com>
2023-03-03 19:58:20 -0300

    funções para verificar e-mail e senha

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
 function verificaEmail(){
     const email = document.getElementById('email');
     const tamEmail = email.value.length;
-
     if (tamEmail <= 0){
         alert("Digite um e-mail válido")
     }
+
 }
 
 function verificaSenha(){
     const senha = document.getElementById('password');
     const tamSenha = senha.value.length;
-
     if (tamSenha <= 0){
-        alert("Digite uma senha válida")
+        alert("Digite uma senha válida");
     }
 }
-addEventListener ("submit", verificaEmail);addEventListener ("submit", verificaSenha);
+
+const btnEmail = document.getElementById('sign-in');
+btnEmail.addEventListener("click", verificaEmail);
+btnEmail.addEventListener("click", verificaSenha);
